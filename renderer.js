8ef8c494f4ede9c22287a77af14cff19a3124ec4
Jeff Wong <jeffmjwong@gmail.com>
2018-06-08 09:09:13 +1000

    Replace placeholder value with real values and populate the template

--- renderer.js
+++ renderer.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 
+function mergeValues(values, content) {
+  // cycle over the keys
+  for (const key in values) {
+    // replace all {{key}} with the value from the values object
+    content = content.replace(`{{ ${key} }}`, values[key]);
+  }
+  // return merged content
+  return content;
+}
+
 function view(templateName, values, response) {
   // read from the template file
-  const fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  let fileContents = fs.readFileSync('./views/' + templateName + '.html', { encoding: 'utf8' });
     // insert values into the content
+  fileContents = mergeValues(values, fileContents);
     // write out to the response
   response.write(fileContents);
 }
