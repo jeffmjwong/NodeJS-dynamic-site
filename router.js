0af33aef26beda16db4b9933972899863ecf154e
Jeff Wong <jeffmjwong@gmail.com>
2018-06-07 17:14:53 +1000

    Display user data and error message from treehouse API

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+const Profile = require("./profile.js");
+
 // Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   if (request.url === '/') {
@@ -16,15 +18,33 @@ function user(request, response) {
   if (username.length > 0) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Header\n');
-    response.write(username + '\n');
-    response.end('Footer\n');
+
+    // get json from treehouse
+    const studentProfile = new Profile(username);
+
+    // on "end"
+    studentProfile.on("end", function(profileJSON) {
+      // show profile
+
+      // store the values which we need
+      const values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      };
+      response.write(values.username + ' has ' + values.badges + ' badges\n');
+      response.end('Footer\n');
+    });
+
+    // on "error"
+    studentProfile.on("error", function(error) {
+      // show error
+      response.write(error.message + '\n');
+      response.end('Footer\n');
+    });
   }
   // if url === '/...'
-  // get json from treehouse
-  // on "end"
-  // show profile
-  // on "error"
-  // show error
 }
 
 module.exports.home = home;
